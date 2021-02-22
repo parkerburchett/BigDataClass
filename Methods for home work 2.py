0e8599ae65b6eb6acda23e8fad6e7efe5f53b1fb
Parker Burchett <parkerburchett@gmail.com>
2021-02-21 16:56:08 -0800

    Added methods for homework2

---
+++ Methods for home work 2.py
@@ -0,0 +1,13 @@
+
+def prob1():
+    values = [[x,(x % 10)] for x in range(1,76)]
+    for v in values:
+        if(v[1] == 0):
+            v[1] = 10
+
+    for v in values:
+        print(v)
+
+def sim_expected_number_of_ones():
+    
+
